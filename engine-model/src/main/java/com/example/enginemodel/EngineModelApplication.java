9f48f23c8de913759b8c351927d641b25811f46e
liudayang1995 <15216658947@163.com>
2020-10-27 17:29:34 +0800

    第一次提交

---
+++ engine-model/src/main/java/com/example/enginemodel/EngineModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.enginemodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineModelApplication.class, args);
+    }
+
+}
